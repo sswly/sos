18680850152cfcec170748b92e3860623b2c3538
sswly <sswlymail@gmail.com>
2012-11-13 13:37:30 +0800

    remove build file

--- communicator/Debug/src_bus.o.d
+++
@@ -1,2 +0,0 @@
-Debug/src_bus.o: /home/shisu/.codelite/sos/communicator/src/bus.c \
- include/fix_queue.h
--- communicator/Debug/test_test_bus.o.d
+++
@@ -1,3 +0,0 @@
-Debug/test_test_bus.o: \
- /home/shisu/.codelite/sos/communicator/test/test_bus.c \
- /home/shisu/.codelite/sos/communicator/test/utf.h include/bus.h
